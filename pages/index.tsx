e3fa8ea5f10953b9cc9996ea26b70a3499d0f5ff
j <jacob@lapworth.nz>
2021-04-05 16:45:45 +1200

    🚧 Add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,12 @@ import Button from 'components/Button'
 import Page from 'components/Page'
 
 export default function Home() {
-  return <Page>Jacob Lapworth – Product designer</Page>
+  return (
+    <Page>
+      <div className="flex items-end h-80">
+        <div>Jacob Lapworth</div>
+      </div>
+      <div className="bg-pink-50 h-80"></div>
+    </Page>
+  )
 }
