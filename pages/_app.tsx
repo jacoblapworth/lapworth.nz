782827b79b92a3f2bf9052f001a3a0f25ced5107
j <jacob@lapworth.nz>
2021-04-05 16:45:45 +1200

    🚧 Add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import Providers from '@/components/Providers'
 import type { AppProps } from 'next/app'
-import React from 'react'
+
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
