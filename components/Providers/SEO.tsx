cb5aea191a70924d82ba260dc8d4342225de55bc
j <jacob@lapworth.nz>
2021-06-13 15:03:37 +1200

    Fix scale

--- components/Providers/SEO.tsx
+++ components/Providers/SEO.tsx
@@ -20,7 +20,7 @@ export default function SEO() {
           rel="icon"
           href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${emoji}</text></svg>`}
         />
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
         <meta
           name="theme-color"
           content="#fff"
