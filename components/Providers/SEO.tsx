003fee8988e9059d269ffed17f6eca72ba44ce20
j <jacob@lapworth.nz>
2021-06-13 14:33:12 +1200

    iOS 15

--- components/Providers/SEO.tsx
+++ components/Providers/SEO.tsx
@@ -1,8 +1,7 @@
+import { defaultSEO } from '@/config/seo'
+import { NextSeo } from 'next-seo'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { NextSeo } from 'next-seo'
-
-import { defaultSEO } from '@/config/seo'
 
 export default function SEO() {
   const router = useRouter()
@@ -21,6 +20,17 @@ export default function SEO() {
           rel="icon"
           href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${emoji}</text></svg>`}
         />
+        <meta name="viewport" content="viewport-fit=cover" />
+        <meta
+          name="theme-color"
+          content="#fff"
+          // media="(prefers-color-scheme: light)"
+        />
+        {/* <meta
+          name="theme-color"
+          content="#0b3e05"
+          media="(prefers-color-scheme: dark)"
+        /> */}
       </Head>
     </>
   )
