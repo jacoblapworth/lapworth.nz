c96d406032a8b7e41ecc7fd76a301d8563c35a56
j <jacob@lapworth.nz>
2021-06-17 10:25:46 +1200

    💄 Button styling

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,14 +1,16 @@
 import Link from 'next/link'
+import { HTMLAttributes } from 'react'
+import cn from 'classnames';
 
-interface Props {
+interface Props extends HTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode
   href?: string
 }
 
-const Button: React.FC<Props> = ({ children, href, ...rest }) => {
+const Button: React.FC<Props> = ({ children, href, className, ...rest }) => {
   return (
     <Link href={href} passHref>
-      <button {...rest}>{children}</button>
+      <button className={cn('hover:opacity-60', className)} {...rest}>{children}</button>
     </Link>
   )
 }
