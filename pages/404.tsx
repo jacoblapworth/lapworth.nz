e65bf0feeb9076dedd6be5dd0011368b334b8ec0
j <jacob@lapworth.nz>
2021-06-17 10:25:57 +1200

    💄404 Page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,16 @@
+import cn from 'classnames'
 import Button from '@components/Button'
 import Page from '@components/Page'
 
 export default function Home() {
   return (
-    <Page>
+    <Page className={cn('p-4')}>
       <h1>404</h1>
-      <p>
+      <p className={cn('pb-4')}>
         This page doesn’t exist. Try heading back home to start from the
         beginning.
       </p>
-      <Button href={'/'}>Back to home.</Button>
+      <Button className={cn('underlines')} href={'/'}>← Back to home.</Button>
     </Page>
   )
 }
