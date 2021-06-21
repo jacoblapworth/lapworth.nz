4b39b7c6ba7f9357f0611e530e3fd5733939332f
j <jacob@lapworth.nz>
2021-06-21 15:27:07 +1200

    Correct config for github/vercel integration

--- next.config.js
+++ next.config.js
@@ -5,9 +5,4 @@ module.exports = {
     locales: ['en-NZ'],
     defaultLocale: 'en-NZ',
   },
-  github: {
-    // https://vercel.com/docs/configuration#git/github-silent
-    // When set to true, Vercel for GitHub will stop commenting on pull requests and commits.
-    silent: true,
-  },
 }
