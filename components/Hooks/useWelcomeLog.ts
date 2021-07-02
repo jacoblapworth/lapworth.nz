ffc38550344134705f93221f76ce55a61d5ec36e
J Lapworth <jacob@lapworth.nz>
2021-07-02 13:11:28 +1200

    📄 Add ASCII

---
+++ components/Hooks/useWelcomeLog.ts
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+
+export function useWelcomeLog() {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      console.info(`
+     ,            /)
+       _   _  ___(/_
+    /_(_(_(__(_)/_)
+ .-/
+(_/
+
+j's kōpaki — portfolio
+
+`)
+    }
+  }, [])
+}
+
+export default useWelcomeLog
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,11 @@ import Providers from '@components/Providers'
 import type { AppProps } from 'next/app'
 
 import '../styles/globals.css'
+import { useWelcomeLog } from '../components/Hooks/useWelcomeLog'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useWelcomeLog()
+
   return (
     <Providers>
       <Component {...pageProps} />
