9b35cce360ee1ee9f32ad4dfae2aa46ba473ad4d
J Lapworth <jacob.lapworth@xero.com>
2021-07-20 13:57:20 +1200

    🧪 Clean up cypress types

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -15,8 +15,9 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
