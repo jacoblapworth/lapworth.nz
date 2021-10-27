791966652ff8ed0968f05438a389bb628338ad8b
jacoblapworth <jacob@lapworth.nz>
2021-10-27 21:03:34 +1300

    Only require environment variable for production

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,11 @@
-const { GOOGLE_MEASUREMENT_ID } = process.env
+let { GOOGLE_MEASUREMENT_ID } = process.env
+
+if (process.env.NODE_ENV !== 'production') {
+  GOOGLE_MEASUREMENT_ID = ''
+}
 
 if (!GOOGLE_MEASUREMENT_ID) {
-  throw new Error('Provide a Google Tag Manager ID')
+  throw new Error('Provide a Google Tag Manager ID: GOOGLE_MEASUREMENT_ID')
 }
 
 export const config = {
