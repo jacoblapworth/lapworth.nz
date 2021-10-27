d887ff0e680adba8dbadcd30075ace5a955d2d5a
jacoblapworth <jacob@lapworth.nz>
2021-10-27 17:01:53 +1300

    Remove Apple Music

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,29 +1,9 @@
-const {
-  APPLE_MUSIC_PRIVATE_KEY,
-  APPLE_TEAM_ID,
-  APPLE_MUSIC_KEY_ID,
-  GOOGLE_MEASUREMENT_ID,
-} = process.env
-
-if (!APPLE_TEAM_ID) {
-  throw new Error('Provide an Apple team ID')
-}
-
-if (!APPLE_MUSIC_KEY_ID) {
-  throw new Error('Provide an Apple Music key ID')
-}
-
-if (!APPLE_MUSIC_PRIVATE_KEY) {
-  throw new Error('Provide an Apple Music private key')
-}
+const { GOOGLE_MEASUREMENT_ID } = process.env
 
 if (!GOOGLE_MEASUREMENT_ID) {
   throw new Error('Provide a Google Tag Manager ID')
 }
 
 export const config = {
-  APPLE_MUSIC_PRIVATE_KEY,
-  APPLE_TEAM_ID,
-  APPLE_MUSIC_KEY_ID,
   GOOGLE_MEASUREMENT_ID,
 }
