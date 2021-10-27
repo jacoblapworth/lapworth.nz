370a0c3a89ff8160c8f5e775c909e4d7e74ded65
jacoblapworth <jacob@lapworth.nz>
2021-10-27 21:40:27 +1300

    Fix 404 double nav

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,10 @@
 import cn from 'classnames'
 
 import Button from '@/components/Button'
-import Page from '@/components/Page'
 
 export default function Home() {
   return (
-    <Page className={cn('p-4')}>
+    <>
       <h1>404</h1>
       <p className={cn('pb-4')}>
         This page doesn’t exist. Try heading back home to start from the
@@ -14,6 +13,6 @@ export default function Home() {
       <Button className={cn('underlines')} href={'/'}>
         ← Back to home.
       </Button>
-    </Page>
+    </>
   )
 }
