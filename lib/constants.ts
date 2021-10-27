3532936744b047cfc5eb2896de9b574c52a1152c
jacoblapworth <jacob@lapworth.nz>
2021-10-27 21:59:25 +1300

    Fix workflow

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,4 @@
-let { GOOGLE_MEASUREMENT_ID } = process.env
-
-if (process.env.NODE_ENV !== 'production') {
-  GOOGLE_MEASUREMENT_ID = ''
-}
+const { GOOGLE_MEASUREMENT_ID } = process.env
 
 if (!GOOGLE_MEASUREMENT_ID) {
   throw new Error('Provide a Google Tag Manager ID: GOOGLE_MEASUREMENT_ID')
