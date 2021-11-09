1c5318504f2430725c455c3032daeaa1e8be08ca
jacoblapworth <jacob@lapworth.nz>
2021-11-10 11:47:17 +1300

    Bullets styling

--- components/Link/index.tsx
+++ components/Link/index.tsx
@@ -1,17 +1,21 @@
 import { FC } from 'react'
 
+import { styled } from '@/styles'
+
 interface Props {
   children?: React.ReactNode
   href?: string
   sameTab?: boolean
 }
 
+const A = styled('a', {})
+
 const Link: FC<Props> = ({ children, href, sameTab, ...rest }) => {
   const target = sameTab ? '' : '_blank'
   return (
-    <a target={target} rel="noopener noreferrer" href={href} {...rest}>
+    <A target={target} rel="noopener noreferrer" href={href} {...rest}>
       {children}
-    </a>
+    </A>
   )
 }
 
