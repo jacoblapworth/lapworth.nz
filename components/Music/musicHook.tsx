3a8c3683467b189a22e0e6092580236fccb25099
jacoblapworth <jacob@lapworth.nz>
2021-11-14 14:53:22 +1300

    Load script

--- components/Music/musicHook.tsx
+++ components/Music/musicHook.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 
+import { config } from '@/lib/constants'
+
 export const useAppleMusic = (developerToken: string) => {
   const [client, setClient] = useState<MusicKit.MusicKitInstance>()
 
@@ -9,7 +11,7 @@ export const useAppleMusic = (developerToken: string) => {
         developerToken,
         app: {
           name: 'Lapworth.nz',
-          icon: 'http://localhost:3000/static/jacob-icon.png',
+          icon: `${config.VERCEL_URL}/static/jacob-icon.png`,
         },
       })
 
--- pages/music/authorise.tsx
+++ pages/music/authorise.tsx
@@ -1,6 +1,8 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 import { GetStaticProps } from 'next'
+import { NextSeo } from 'next-seo'
+import NextScript from 'next/script'
 
 import { createAppleJWT } from '@/components/Music'
 import { useAppleMusic } from '@/components/Music/musicHook'
@@ -10,17 +12,27 @@ interface PageProps {
 }
 
 export default function Home({ developerToken }: PageProps) {
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => setMounted(true), [])
+
   const appleMusic = useAppleMusic(developerToken)
 
+  if (!mounted) return null
+
   const musicUserToken = appleMusic?.musicUserToken
+
   if (!musicUserToken) {
-    appleMusic?.unauthorize()
     appleMusic?.authorize()
+  } else {
+    console.log({ musicUserToken })
   }
 
-  console.log({ musicUserToken })
-
-  return null
+  return (
+    <>
+      <NextSeo noindex={true} />
+      <NextScript src="https://js-cdn.music.apple.com/musickit/v1/musickit.js" />
+    </>
+  )
 }
 
 export const getStaticProps: GetStaticProps<PageProps> = async () => {
