79101c81d70e383e14ab582aab5adeb920cb4ff0
jacoblapworth <jacob@lapworth.nz>
2021-11-20 13:39:10 +1300

    Add work experience

---
+++ components/Work/Experience.tsx
@@ -0,0 +1,86 @@
+import React, { FC } from 'react'
+
+import { styled } from '@/styles'
+
+import Link from '../Link'
+
+interface ExperienceRowProps {
+  title?: string
+  workplace: string
+  href: string
+  location?: string
+  from: string
+  to: string
+  isCurrent?: boolean
+}
+
+const Column = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+const Row = styled('div', {
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  gap: '1rem',
+})
+
+const ExperienceRow: FC<ExperienceRowProps> = ({
+  workplace,
+  from,
+  to,
+  href,
+}) => {
+  return (
+    <Row>
+      <Link href={href}>{workplace}</Link>
+      <Column>
+        <div>{from}</div>
+        <div>— {to}</div>
+      </Column>
+    </Row>
+  )
+}
+
+const Stack = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '16px',
+  justifyContent: 'space-between',
+  maxWidth: '12rem',
+})
+
+interface ExperienceProps {}
+
+export const Experience: FC<ExperienceProps> = ({}) => {
+  return (
+    <Stack>
+      <ExperienceRow
+        workplace="Xero"
+        href="https://www.xero.com/nz/"
+        from="2020"
+        to="Now"
+      />
+      <ExperienceRow
+        workplace="Vend"
+        href="https://www.vendhq.com/nz/"
+        from="2019"
+        to="2020"
+      />
+      <ExperienceRow
+        workplace="Timely"
+        href="https://www.gettimely.com/"
+        from="2018"
+        to="2019"
+      />
+      <ExperienceRow
+        workplace="Trade Me"
+        href="https://www.trademe.co.nz/a/"
+        from="2016"
+        to="2018"
+      />
+    </Stack>
+  )
+}
+
+export default Experience
