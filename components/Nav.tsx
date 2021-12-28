aba83dedb91bf0dd4486cbdfaa5d9ebea9835474
J <jacob@lapworth.nz>
2021-12-28 00:45:50 +0000

    add border highlight to active nav link

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,17 +2,31 @@ import { FC, useState } from 'react'
 
 import { motion } from 'framer-motion'
 import NextLink, { LinkProps as NextLinkProps } from 'next/link'
+import { useRouter } from 'next/router'
 
 import { styled } from '@/styles'
 
 import Link from './Link'
 
 const A = styled('a', {
+  $$highlightWidth: '20px',
   paddingBlock: 8,
   borderBottom: '1px solid $divider',
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
+  position: 'relative',
+
+  '&::after': {
+    content: '',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    height: '2px',
+    width: '0px',
+    backgroundColor: '$divider',
+    transition: 'width 0.1s ease-in-out',
+  },
 
   '&:first-child': {
     borderTop: '1px solid $divider',
@@ -20,11 +34,26 @@ const A = styled('a', {
 
   '&:hover': {
     backgroundColor: '$surfaceHovered',
+
+    '&::after': {
+      width: '$$highlightWidth',
+    },
+  },
+
+  variants: {
+    isActive: {
+      true: {
+        '&::after': {
+          width: '$$highlightWidth',
+        },
+      },
+    },
   },
 })
 
 const NavLink: FC<NextLinkProps> = ({ children, href }) => {
   const [animate, setAnimate] = useState('hidden')
+  const router = useRouter()
 
   const variants = {
     visible: { rotate: 0, opacity: 1 },
@@ -39,9 +68,15 @@ const NavLink: FC<NextLinkProps> = ({ children, href }) => {
     setAnimate('hidden')
   }
 
+  const isActive = router.pathname === href
+
   return (
     <NextLink href={href} passHref>
-      <A onPointerOver={onPointerOver} onPointerLeave={onPointerLeave}>
+      <A
+        onPointerOver={onPointerOver}
+        onPointerLeave={onPointerLeave}
+        isActive={isActive}
+      >
         {children}
         <motion.div
           variants={variants}
