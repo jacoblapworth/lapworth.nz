7a0a159dd82c07460150e4dba619dae0be65c13f
jacoblapworth <jacob@lapworth.nz>
2022-05-14 15:27:29 +1200

    Test dark mode toggle

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -8,3 +8,15 @@ describe('Home', () => {
     cy.contains('-36.862600º, 174.741270º')
   })
 })
+
+describe('Darkmode', () => {
+  before(() => {
+    cy.visit('/')
+  })
+
+  it('should toggle dark mode', () => {
+    cy.get('body').should('have.css', 'background-color', 'rgb(246, 246, 247)')
+    cy.findByTestId('theme-toggle').click()
+    cy.get('body').should('have.css', 'background-color', 'rgb(11, 12, 13)')
+  })
+})
