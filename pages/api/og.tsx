1937cb90d175e4e8c72b3e5a42c220534b365c28
J <jacob@lapworth.nz>
2022-10-18 20:53:54 +1300

    Add g scaffold

---
+++ pages/api/og.tsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from '@vercel/og'
+import { NextApiHandler } from 'next'
+
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+export const OGImage: NextApiHandler = async (req, res) => {
+  const { query } = req
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          textAlign: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Hello world!
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    },
+  )
+}
+
+export default OGImage
