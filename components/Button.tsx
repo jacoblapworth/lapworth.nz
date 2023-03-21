783ea8d5569c922776e42b4c483608a2de9bacfb
J <jacob@lapworth.nz>
2023-03-21 18:33:10 +1300

    Update 404 (#902)

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,23 +1,5 @@
-import { HTMLAttributes, PropsWithChildren } from 'react'
-
 import NextLink from 'next/link'
 
-interface Props extends HTMLAttributes<HTMLButtonElement> {
-  children?: React.ReactNode
-  href: string
-}
+import { styled } from '@/styles'
 
-export const Button: React.FC<PropsWithChildren<Props>> = ({
-  children,
-  href,
-  className,
-  ...rest
-}) => {
-  return (
-    <NextLink href={href} passHref legacyBehavior>
-      <button className={className} {...rest}>
-        {children}
-      </button>
-    </NextLink>
-  )
-}
+export const Button = styled(NextLink, {})
