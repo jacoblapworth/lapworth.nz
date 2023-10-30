a5f1ebd68dd328a9ea5f50887eecbb7b685fc23e
J <jacob@lapworth.nz>
2023-10-30 15:53:10 +1300

    remove inaccessible viewport settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,8 +51,6 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  minimumScale: 1,
-  userScalable: false,
   themeColor: [
     {
       media: '(prefers-color-scheme: dark)',
