5b3733e7d64d43ec4ddb5258b5b0ea0d5d88ff99
J <jacob@lapworth.nz>
2023-10-30 17:53:12 +1300

    restore vercel analytics (#1373)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './index.css'
 
 import { ReactNode } from 'react'
 
+import { Analytics } from '@vercel/analytics/react'
 import { Metadata, Viewport } from 'next'
 import localFont from 'next/font/local'
 import { ServerThemeProvider } from 'next-themes'
@@ -77,6 +78,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         </head>
         <body className={css({ bg: 'background' })}>
           <Page>{children}</Page>
+          <Analytics />
         </body>
       </html>
     </ServerThemeProvider>
