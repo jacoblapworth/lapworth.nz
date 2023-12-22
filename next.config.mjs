927c125b088b2628ecdfcd1c41157560273c3c3b
J <jacob@lapworth.nz>
2023-12-22 16:24:06 +1300

    refactor: mdx code config

--- next.config.mjs
+++ next.config.mjs
@@ -1,43 +1,11 @@
-import fs from 'fs'
-import path from 'path'
-
 import createMDX from '@next/mdx'
 import rehypePrettyCode from 'rehype-pretty-code'
 import rehypeSlug from 'rehype-slug'
 import remarkGfm from 'remark-gfm'
 
-/** @type {import('rehype-pretty-code').Options}*/
 const prettyCodeOptions = {
-  // Use one of Shiki's packaged themes
-  // theme: 'one-dark-pro',
-  // Or your own JSON theme
-  theme: JSON.parse(
-    fs.readFileSync(
-      path.join(process.cwd(), 'assets/moonlight-ii.json'),
-      'utf-8',
-    ),
-  ),
-
-  // Keep the background or use a custom background color?
+  theme: 'github-dark',
   keepBackground: true,
-
-  // Callback hooks to add custom logic to nodes when visiting
-  // them.
-  onVisitLine(node) {
-    // Prevent lines from collapsing in `display: grid` mode, and
-    // allow empty lines to be copy/pasted
-    if (node.children.length === 0) {
-      node.children = [{ type: 'text', value: ' ' }]
-    }
-  },
-  onVisitHighlightedLine(node) {
-    // Each line node by default has `class="line"`.
-    node.properties.className.push('highlighted')
-  },
-  onVisitHighlightedWord(node) {
-    // Each word node has no className by default.
-    node.properties.className = ['word']
-  },
 }
 
 /** @type {import('next').NextConfig}*/
@@ -67,7 +35,6 @@ const config = {
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  experimental: {},
 }
 
 const withMDX = createMDX({
