83b91ae7ccb09beb582c01daa0fac65c9565377c
J <jacob@lapworth.nz>
2024-01-23 13:02:12 +1300

    wip

--- app/work/vend/tabs/page.tsx
+++ app/work/vend/tabs/page.tsx
@@ -1,24 +1,23 @@
-import { images } from '@/app/work/vend/images'
-import { TabsExample } from '@/app/work/vend/VendTabs'
+// import { images } from '@/app/work/vend/images'
+// import { TabsExample } from '@/app/work/vend/VendTabs'
 import { Text } from '@/components/Typography'
-import { prepareMDX } from '@/lib/markdown'
+// import { prepareMDX } from '@/lib/markdown'
 
 export default async function Page() {
-  const { content, frontmatter } = await prepareMDX({
-    fileName: 'tabs',
-    directory: 'app/work/vend/tabs',
-    options: {
-      scope: { images },
-    },
-    components: { TabsExample },
-  })
+  // const { content, frontmatter } = await prepareMDX({
+  //   fileName: 'tabs',
+  //   directory: 'app/work/vend/tabs',
+  //   options: {
+  //     scope: { images },
+  //   },
+  //   components: { TabsExample },
+  // })
 
   return (
     <>
       <Text as="h1" display size="large">
-        {frontmatter.title}
+        {/* {frontmatter.title} */}Test
       </Text>
-      {content}
     </>
   )
 }
