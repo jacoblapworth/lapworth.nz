c5127c5bacfd496f8ae04c5f2043c30572d66626
J <jacob@lapworth.nz>
2024-05-17 17:24:01 +1200

    add mdxts

--- next.config.mjs
+++ next.config.mjs
@@ -1,45 +1,11 @@
-import fs from 'fs'
-import path from 'path'
-
 import withBundleAnalyzer from '@next/bundle-analyzer'
-import createMDX from '@next/mdx'
 import { withSentryConfig } from '@sentry/nextjs'
-import rehypePrettyCode from 'rehype-pretty-code'
-import rehypeSlug from 'rehype-slug'
-import remarkGfm from 'remark-gfm'
-/** @type {import('rehype-pretty-code').Options}*/
-const prettyCodeOptions = {
-  // Use one of Shiki's packaged themes
-  // theme: 'one-dark-pro',
-  // Or your own JSON theme
-  theme: JSON.parse(
-    fs.readFileSync(
-      path.join(process.cwd(), 'assets/moonlight-ii.json'),
-      'utf-8',
-    ),
-  ),
+import { createMdxtsPlugin } from 'mdxts/next'
 
-  // Keep the background or use a custom background color?
-  keepBackground: true,
-
-  // Callback hooks to add custom logic to nodes when visiting
-  // them.
-  onVisitLine(node) {
-    // Prevent lines from collapsing in `display: grid` mode, and
-    // allow empty lines to be copy/pasted
-    if (node.children.length === 0) {
-      node.children = [{ type: 'text', value: ' ' }]
-    }
-  },
-  onVisitHighlightedLine(node) {
-    // Each line node by default has `class="line"`.
-    node.properties.className.push('highlighted')
-  },
-  onVisitHighlightedWord(node) {
-    // Each word node has no className by default.
-    node.properties.className = ['word']
-  },
-}
+const withMdxts = createMdxtsPlugin({
+  theme: 'nord',
+  gitSource: 'https://github.com/jacoblapworth/lapworth.nz',
+})
 
 /** @type {import('next').NextConfig}*/
 const config = {
@@ -73,54 +39,45 @@ const config = {
   },
 }
 
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypeSlug, [rehypePrettyCode, prettyCodeOptions]],
-    providerImportSource: '@mdx-js/react',
-  },
-})
+export default withMdxts(
+  withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })(
+    withSentryConfig(
+      config,
+      {
+        // For all available options, see:
+        // https://github.com/getsentry/sentry-webpack-plugin#options
+        silent: true,
+        org: 'jacoblapworth',
+        project: 'lapworth',
+      },
+      {
+        // For all available options, see:
+        // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-export default withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })(
-  withSentryConfig(
-    withMDX(config),
-    {
-      // For all available options, see:
-      // https://github.com/getsentry/sentry-webpack-plugin#options
+        // Upload a larger set of source maps for prettier stack traces (increases build time)
+        widenClientFileUpload: true,
 
-      // Suppresses source map uploading logs during build
-      silent: true,
-      org: 'jacoblapworth',
-      project: 'lapworth',
-    },
-    {
-      // For all available options, see:
-      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+        // Transpiles SDK to be compatible with IE11 (increases bundle size)
+        transpileClientSDK: true,
 
-      // Upload a larger set of source maps for prettier stack traces (increases build time)
-      widenClientFileUpload: true,
+        // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+        // This can increase your server load as well as your hosting bill.
+        // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+        // side errors will fail.
+        tunnelRoute: '/monitoring',
 
-      // Transpiles SDK to be compatible with IE11 (increases bundle size)
-      transpileClientSDK: true,
+        // Hides source maps from generated client bundles
+        hideSourceMaps: true,
 
-      // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-      // This can increase your server load as well as your hosting bill.
-      // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-      // side errors will fail.
-      tunnelRoute: '/monitoring',
+        // Automatically tree-shake Sentry logger statements to reduce bundle size
+        disableLogger: true,
 
-      // Hides source maps from generated client bundles
-      hideSourceMaps: true,
-
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      disableLogger: true,
-
-      // Enables automatic instrumentation of Vercel Cron Monitors.
-      // See the following for more information:
-      // https://docs.sentry.io/product/crons/
-      // https://vercel.com/docs/cron-jobs
-      automaticVercelMonitors: true,
-    },
+        // Enables automatic instrumentation of Vercel Cron Monitors.
+        // See the following for more information:
+        // https://docs.sentry.io/product/crons/
+        // https://vercel.com/docs/cron-jobs
+        automaticVercelMonitors: true,
+      },
+    ),
   ),
 )
