c39307e74e857f17ceeedb6faed7ef985c9f19ac
Jacob Lapworth <jacob.lapworth@xero.com>
2024-07-06 12:19:09 +1200

    feat: create sanity

--- lib/env.ts
+++ lib/env.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-const envVariables = z.object({
+const EnvSchema = z.object({
   APPLE_MUSIC_PRIVATE_KEY: z.string(),
   APPLE_TEAM_ID: z.string(),
   APPLE_MUSIC_KEY_ID: z.string(),
@@ -14,14 +14,17 @@ const envVariables = z.object({
   ] as const),
   NEXT_PUBLIC_PRODUCTION_URL: z.string(),
   NEXT_PUBLIC_SENTRY_DSN: z.string(),
+  NEXT_PUBLIC_SANITY_API_VERSION: z.string(),
+  NEXT_PUBLIC_SANITY_DATASET: z.string(),
+  NEXT_PUBLIC_SANITY_PROJECT_ID: z.string(),
 })
 
-envVariables.parse(process.env)
+EnvSchema.parse(process.env)
 
 declare global {
   namespace NodeJS {
     // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface ProcessEnv extends z.infer<typeof envVariables> {}
+    interface ProcessEnv extends z.infer<typeof EnvSchema> {}
   }
 
   namespace MusicKit {
