47d921f40640e5e4d218d71730ba6798505f5caf
Jacob Lapworth <jacob.lapworth@xero.com>
2025-01-14 12:40:32 +1300

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,75 +1,104 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import prettier from 'eslint-plugin-prettier'
-import cypress from 'eslint-plugin-cypress'
-import tsParser from '@typescript-eslint/parser'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
+import url from 'node:url'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginJs from '@eslint/js'
+import tsParser from '@typescript-eslint/parser'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import pluginImportX from 'eslint-plugin-import-x'
+import pluginPromise from 'eslint-plugin-promise'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactCompiler from 'eslint-plugin-react-compiler'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+const compat = new FlatCompat({ baseDirectory: __dirname })
 
 export default [
-  eslintJs.configs.recommended,
-  ...eslintTypeScript.configs.recommended,
-  ...cypress.configs.recommended,
-  ...typescriptEslint.configs.recommended,
-  ...prettier.configs.recommended,
-  ...compat.extends('next/core-web-vitals', 'plugin:cypress/recommended'),
   {
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-      prettier,
-      cypress,
-    },
-
-    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-    ...eslintPluginPrettierRecommended,
-
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+  },
+  {
     languageOptions: {
-      globals: {
-        ...cypress.environments.globals.globals,
-      },
-
+      ecmaVersion: 'latest',
+      globals: { ...globals.browser, ...globals.node },
+      sourceType: 'module',
       parser: tsParser,
-      ecmaVersion: 5,
-      sourceType: 'script',
-
       parserOptions: {
+        // projectService: true,
         project: true,
-        tsconfigRootDir: '__dirname',
+        tsconfigRootDir: import.meta.dirname,
       },
     },
 
+    plugins: {
+      'react-compiler': pluginReactCompiler,
+    },
+
+    settings: {
+      'import-x/parsers': {
+        '@typescript-eslint/parser': ['**/*.ts', '**/*.tsx'],
+      },
+
+      'import-x/resolver': {
+        typescript: true,
+        node: true,
+      },
+
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  pluginImportX.flatConfigs.recommended,
+  pluginImportX.flatConfigs.typescript,
+  pluginJs.configs.recommended, // https://github.com/eslint/eslint
+  ...tseslint.configs.strict, // https://github.com/typescript-eslint/typescript-eslint
+  ...tseslint.configs.stylistic,
+  pluginPromise.configs['flat/recommended'], // https://github.com/eslint-community/eslint-plugin-promise
+  pluginReact.configs.flat.recommended, // https://github.com/jsx-eslint/eslint-plugin-react
+  pluginReact.configs.flat['jsx-runtime'], // https://github.com/jsx-eslint/eslint-plugin-react
+  eslintConfigPrettier, // https://github.com/prettier/eslint-config-prettier
+  ...compat.extends('next'),
+  {
     rules: {
-      '@typescript-eslint/no-misused-promises': [
+      'react/prop-types': 'off',
+      'newline-before-return': 'error',
+      '@typescript-eslint/no-unused-expressions': [
         'error',
         {
-          checksVoidReturn: false,
+          allowTernary: true,
         },
       ],
-
-      '@typescript-eslint/no-unused-vars': 'warn',
-
-      '@typescript-eslint/no-namespace': [
-        'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
         {
-          allowDeclarations: true,
-          allowDefinitionFiles: true,
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
         },
       ],
-
-      'import/order': [
+      '@typescript-eslint/no-empty-object-type': [
         'error',
         {
+          allowInterfaces: 'with-single-extends',
+        },
+      ],
+      '@typescript-eslint/no-empty-function': 'warn',
+      'react-compiler/react-compiler': 'warn',
+      'import-x/no-named-as-default': 'error',
+      'import-x/order': [
+        'error',
+        {
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'always',
           groups: ['builtin', 'external', 'internal'],
-
           pathGroups: [
             {
               pattern: 'react',
@@ -87,16 +116,25 @@ export default [
               position: 'before',
             },
           ],
-
-          pathGroupsExcludedImportTypes: ['react'],
-          'newlines-between': 'always',
-
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
-          },
         },
       ],
     },
   },
+  // ! ===================== DISCLAIMER =====================
+  // ! There is no official solution available for new ESLint 9 flat config structure for NextJS
+  // ! The solution is taken from the community and may not be the best practice, use it at your own risk
+  // ? Ref: https://github.com/vercel/next.js/discussions/49337?sort=top#discussioncomment-5998603
+  // ! ======================================================
+  // {
+  //   plugins: {
+  //     '@next/next': pluginNext,
+  //   },
+  //   rules: {
+  //     ...pluginNext.configs.recommended.rules,
+  //     ...pluginNext.configs['core-web-vitals'].rules,
+  //   },
+  // },
+  // {
+  //   ignores: ['.next/*'],
+  // },
 ]
