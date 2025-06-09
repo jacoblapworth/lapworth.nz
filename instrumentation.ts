88685cf59e35409f20e9cf0241467144f674e30e
J <jacob@lapworth.nz>
2025-06-09 17:28:28 +1200

    chore: upgrade sentry (#2501)

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,24 +1,13 @@
 import * as Sentry from '@sentry/nextjs'
 
-export function register() {
-  Sentry.init({
-    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
-    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-    // spotlight: process.env.NODE_ENV === 'development',
-    // integrations: (integrations) => {
-    //   console.log(integrations)
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await import('./sentry.server.config')
+  }
 
-    //   return [
-    //     Sentry.requestDataIntegration(),
-    //     // Sentry.replayIntegration({
-    //     //   maskAllText: true,
-    //     //   blockAllMedia: true,
-    //     // }),
-    //   ]
-    // },
-  })
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    await import('./sentry.edge.config')
+  }
 }
+
+export const onRequestError = Sentry.captureRequestError
--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import withBundleAnalyzer from '@next/bundle-analyzer'
 import createMDX from '@next/mdx'
 import { withSentryConfig } from '@sentry/nextjs'
 import { NextConfig } from 'next'
-import rehypePrettyCode, { Options } from 'rehype-pretty-code'
-import rehypeSlug from 'rehype-slug'
+import { Options } from 'rehype-pretty-code'
 import remarkGfm from 'remark-gfm'
 
 import theme from './assets/moonlight-ii.json'
@@ -61,41 +60,35 @@ const withMDX = createMDX({
 
 export default withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })(
   withSentryConfig(withMDX(config), {
-    // silent: true,
     org: 'jacoblapworth',
     project: 'lapworth',
-    telemetry: false,
+
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
+
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    tunnelRoute: '/monitoring',
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+
     sourcemaps: {
       deleteSourcemapsAfterUpload: true,
     },
   }),
 )
-
-// {
-//   // For all available options, see:
-//   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-//   // Upload a larger set of source maps for prettier stack traces (increases build time)
-//   widenClientFileUpload: true,
-
-//   // Transpiles SDK to be compatible with IE11 (increases bundle size)
-//   transpileClientSDK: true,
-
-//   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-//   // This can increase your server load as well as your hosting bill.
-//   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-//   // side errors will fail.
-//   tunnelRoute: '/monitoring',
-
-//   // Hides source maps from generated client bundles
-//   hideSourceMaps: true,
-
-//   // Automatically tree-shake Sentry logger statements to reduce bundle size
-//   disableLogger: true,
-
-//   // Enables automatic instrumentation of Vercel Cron Monitors.
-//   // See the following for more information:
-//   // https://docs.sentry.io/product/crons/
-//   // https://vercel.com/docs/cron-jobs
-//   automaticVercelMonitors: true,
-// },
