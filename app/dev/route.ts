877c2a38754173a3d599111ed37f0a1104469910
J <jacob@lapworth.nz>
2025-06-09 18:33:36 +1200

    chore: update next (#2502)
    * chore: update next
    
    * fix: secure cookie

--- app/dev/route.ts
+++ app/dev/route.ts
@@ -1,8 +1,14 @@
+import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
-export function GET() {
-  return NextResponse.json(
-    { isPreview: true },
-    { headers: { 'Set-Cookie': 'preview=true' } },
-  )
+export async function GET() {
+  const store = await cookies()
+  store.set('preview', 'true', {
+    path: '/',
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  })
+
+  return NextResponse.json({ isPreview: true })
 }
