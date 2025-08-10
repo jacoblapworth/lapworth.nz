664818f488304d639197a72b13a9d39aa3616c31
J <jacob@lapworth.nz>
2025-08-11 11:16:20 +1200

    refactor(mdx): next config (#2601)

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,21 +3,12 @@ import { NextConfig } from 'next'
 import withBundleAnalyzer from '@next/bundle-analyzer'
 import createMDX from '@next/mdx'
 import { withSentryConfig } from '@sentry/nextjs'
-import { Options } from 'rehype-pretty-code'
+import rehypePrettyCode, { Options } from 'rehype-pretty-code'
+import rehypeSlug from 'rehype-slug'
 import remarkGfm from 'remark-gfm'
 
-import theme from './src/assets/moonlight-ii.json'
-
 const prettyCodeOptions: Options = {
-  theme: theme as unknown as Options['theme'],
-  keepBackground: true,
-  onVisitLine(node) {
-    // Prevent lines from collapsing in `display: grid` mode, and
-    // allow empty lines to be copy/pasted
-    if (node.children.length === 0) {
-      node.children = [{ type: 'text', value: ' ' }]
-    }
-  },
+  theme: 'github-dark',
 }
 
 const config: NextConfig = {
@@ -54,7 +45,7 @@ const withMDX = createMDX({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [remarkGfm],
-    // rehypePlugins: [rehypeSlug, [rehypePrettyCode, prettyCodeOptions]],
+    rehypePlugins: [rehypeSlug, [rehypePrettyCode, prettyCodeOptions]],
     providerImportSource: '@mdx-js/react',
   },
 })
