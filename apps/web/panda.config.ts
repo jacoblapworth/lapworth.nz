4be8ce761fee94aec83ddc34b0ee52362448cbe6
J <jacob@lapworth.nz>
2025-09-26 15:13:50 +1200

    Add image lightbox dialog (#2722)

--- apps/web/panda.config.ts
+++ apps/web/panda.config.ts
@@ -5,6 +5,14 @@ import { globalCss, semanticTokens, tokens } from './src/styles'
 export default defineConfig({
   conditions: {
     extend: {
+      active: '&:not(:disabled):is(:active, [data-active])',
+      activeItem: '&[data-active-item]',
+      disabled: '&[aria-disabled]',
+      enter: '&[data-enter]',
+      exit: '&[data-exit]',
+      focus: '&:not(:disabled):is(:focus, [data-focus])',
+      focusWithin: '&:not(:disabled):focus-within',
+      hover: '&:not(:disabled):is(:hover, [data-hover])',
       marker: '&::marker, &::-webkit-details-marker',
     },
   },
--- apps/web/src/components/mdx/Image.tsx
+++ apps/web/src/components/mdx/Image.tsx
@@ -1,5 +1,105 @@
+import * as Ariakit from '@ariakit/react'
+import { XIcon } from 'lucide-react'
 import NextImage from 'next/image'
 import { cva } from '@/styled/css'
+import { styled } from '@/styled/jsx'
+
+const DialogDisclosure = styled(Ariakit.DialogDisclosure, {
+  base: {
+    '&:hover': {
+      filter: 'brightness(0.9)',
+    },
+    cursor: 'zoom-in',
+    display: 'inline-block',
+  },
+})
+
+const Backdrop = styled('div', {
+  base: {
+    _enter: {
+      backdropBlur: 'sm',
+      opacity: 1,
+    },
+
+    _exit: {
+      opacity: 1,
+      transform: 'scale(1)',
+    },
+    backdropFilter: 'auto',
+    backgroundColor: 'background/70',
+    filter: 'auto',
+    inset: '0',
+    opacity: 0,
+    position: 'fixed',
+    transitionDuration: '150ms',
+    transitionProperty: 'opacity, backdrop-filter',
+    transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+    zIndex: 100,
+  },
+})
+
+const Dialog = styled(
+  Ariakit.Dialog,
+  {
+    base: {
+      _enter: {
+        opacity: 1,
+        transform: 'scale(1)',
+      },
+      '--inset': '2rem',
+      alignItems: 'stretch',
+      backgroundColor: 'background',
+      boxShadow: `
+      inset 0 0 0 1px token(colors.divider),
+      0 25px 30px -10px rgb(0 0 0 / 0.15);
+    `,
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '1rem',
+      height: 'fit-content',
+      inset: 'var(--inset)',
+      justifyContent: 'stretch',
+      margin: 'auto',
+      maxHeight: 'calc(100dvh - var(--inset) * 2)',
+      maxWidth: '80vw',
+      opacity: 0,
+      position: 'fixed',
+      transform: 'scale(0.95)',
+      transformOrigin: 'center',
+      transitionDuration: '100ms',
+      transitionProperty: 'all',
+      transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+      zIndex: 800,
+    },
+  },
+  {
+    defaultProps: {
+      backdrop: <Backdrop />,
+    },
+  },
+)
+
+const Dismiss = styled(
+  Ariakit.DialogDismiss,
+  {
+    base: {
+      _hover: {
+        backgroundColor: 'primary',
+        color: 'background',
+      },
+      border: 'primary',
+      color: 'primary',
+      cursor: 'pointer',
+      position: 'absolute',
+      top: '-2rem',
+    },
+  },
+  {
+    defaultProps: {
+      children: <XIcon aria-label="Close dialog" />,
+    },
+  },
+)
 
 const styles = cva({
   base: {
@@ -7,7 +107,6 @@ const styles = cva({
       marginBlockStart: -1,
     },
     border: 'muted',
-    // boxSizing: 'border-box',
     height: 'auto',
     maxWidth: '100%',
   },
@@ -15,12 +114,26 @@ const styles = cva({
 
 export function Image(props: React.ComponentProps<typeof NextImage>) {
   return (
-    <NextImage
-      className={styles()}
-      height={400}
-      placeholder={props.blurDataURL ? 'blur' : undefined}
-      width={800}
-      {...props}
-    />
+    <Ariakit.DialogProvider>
+      <DialogDisclosure>
+        <NextImage
+          className={styles()}
+          height={400}
+          placeholder={props.blurDataURL ? 'blur' : undefined}
+          width={800}
+          {...props}
+        />
+      </DialogDisclosure>
+      <Dialog unmountOnHide>
+        <Dismiss />
+        <NextImage
+          alt={props.alt}
+          height={800}
+          placeholder={props.blurDataURL ? 'blur' : undefined}
+          src={props.src}
+          width={1600}
+        />
+      </Dialog>
+    </Ariakit.DialogProvider>
   )
 }
