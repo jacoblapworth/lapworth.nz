da1f802fce4f2c67ae4bf5db8b6daa0c386f6f70
Jacob Lapworth <jacob@lapworth.nz>
2025-10-16 14:55:38 +0100

    feat: cv

---
+++ apps/web/src/app/cv/experience-list-item.tsx
@@ -0,0 +1,67 @@
+import { Link } from '@/components/Link'
+import { Text } from '@/components/Typography'
+import { Box, Stack, styled, VStack } from '@/styled/jsx'
+import type { ExperienceItem } from './experience'
+
+const Ul = styled('ul', {
+  base: {
+    // listStyle:
+    listStylePosition: 'outside',
+    listStyleType: 'square',
+    marginInlineStart: 16,
+  },
+})
+
+const Li = styled('li', {
+  base: {
+    _marker: {
+      color: 'tertiary',
+    },
+    paddingInlineStart: 4,
+  },
+})
+
+export function ExperienceListItem({ item }: { item: ExperienceItem }) {
+  return (
+    <Box
+      display="grid"
+      gridColumn="1/-1"
+      gridTemplateAreas={`"title" "details"`}
+      md={{
+        gridTemplateAreas: 'unset',
+        gridTemplateColumns: 'subgrid',
+        gridTemplateRows: 'subgrid',
+      }}
+    >
+      <VStack
+        alignItems="start"
+        fontVariantNumeric="oldstyle-nums"
+        gap={0}
+        gridArea="title"
+      >
+        <Text as="p" bold>
+          {item.start} — {item.end ? item.end : 'Present'}
+        </Text>
+      </VStack>
+      <VStack alignItems="start" gap={8} gridArea="details">
+        <VStack alignItems="start" gap={0}>
+          <Text as="h2" bold size="md">
+            {item.role.title}
+          </Text>
+
+          {item.company.href ? (
+            <Link href={item.company.href}>{item.company.title}</Link>
+          ) : (
+            <p>{item.company.title}</p>
+          )}
+        </VStack>
+        <Ul>
+          {item.details.map((detail, index) => (
+            // biome-ignore lint/suspicious/noArrayIndexKey: static list
+            <Li key={index}>{detail}</Li>
+          ))}
+        </Ul>
+      </VStack>
+    </Box>
+  )
+}
---
+++ apps/web/src/app/cv/experience.ts
@@ -0,0 +1,75 @@
+export interface ExperienceItem {
+  company: {
+    title: string
+    href?: string
+  }
+  role: {
+    title: string
+    detail?: string
+  }
+  start: number
+  end?: number
+  url?: string
+  details: string[]
+}
+
+export const xero: ExperienceItem = {
+  company: {
+    href: 'https://xero.com',
+    title: 'Xero',
+  },
+  details: [
+    'Scaled the design system at Xero to support over 200 designers, 1,000 engineers and 100 product teams.',
+    'Developed Design Tokens to support a complete redesign of Xero’s product, delivering on brand promises in marketing and enabling a cohesive experience across web and mobile platforms.',
+    'Redesigned advanced tables across Xero for viewing, filtering and editing, empowering customers to easily scan, compare, and interact with their wealth of financial data.',
+    'Iterative prototyping-lead design of dashboard and widgets for a reimagined homepage experience, the landing page for millions of Xero’s customers.',
+    'Lead user research into pain-points across the product including inconsistent experiences, and issues with density and efficiency, informing design and product strategy.',
+    'Designed and built internal tooling including Figma plugins for Accessibility and Design System documentation.',
+  ],
+  end: 2025,
+  role: {
+    title: 'Senior Product Designer',
+  },
+  start: 2020,
+}
+
+export const vend: ExperienceItem = {
+  company: {
+    href: 'https://www.lightspeedhq.co.uk/vend/',
+    title: 'Vend',
+  },
+  details: [
+    'Design of Vend’s iPad and Web “Easy to learn, easy to use” POS',
+    'Launched accounting integrations with Xero, QuickBooks Online and MYOB, simplifying the accounting process for thousands of retailers.',
+    'Developed product strategies for mobile expansion, identifying opportunities for new apps and features increasing engagement. (Scanner)',
+    'Designed an add-ons platform for 3rd party integrations, that leverages and expands the current product solving complex needs for retailers with unique workflows and enterprise customers.',
+  ],
+  end: 2020,
+  role: {
+    detail: 'Mobile & Web',
+    title: 'Product Designer',
+  },
+
+  start: 2019,
+}
+
+export const trademe: ExperienceItem = {
+  company: {
+    href: 'https://trademe.co.nz',
+    title: 'Trade Me',
+  },
+  details: [
+    'Design of Trade Me’s iOS and Android apps (combined 1.2M MAU), optimising engagement, conversion and retention.',
+    'Lead the mobile design guild, facilitating collaboration across business units, working with product owners and designers, identifying shared problems to resolve cohesive experiences for customers.',
+    'Development of design systems, combining brand and development including developing guidance for onboarding, change aversion, experimentation, and cross platform cohesive experiences.',
+    'Design sprints, Qualitative and quantitative research, identifying user needs, wireframing, prototyping, user- testing, interface design and release management.',
+  ],
+  end: 2018,
+  role: {
+    detail: 'Native iOS & Android',
+    title: 'Product Designer',
+  },
+  start: 2016,
+}
+
+export const experience = [xero, vend, trademe]
