d656eb80e5d79e0130daafc0cc885fe5fb302e33
J <jacob@lapworth.nz>
2025-10-28 12:44:46 +0000

    video

--- apps/web/src/components/mdx/video.tsx
+++ apps/web/src/components/mdx/video.tsx
@@ -4,17 +4,16 @@ import { styled } from '@/styled/jsx'
 const StyledVideo = styled(NextVideo, {
   base: {
     border: 'muted',
-    maxWidth: '100%',
+    maxWidth: '800px',
+    width: '100%',
   },
 })
 
 interface Prop {
   video: string
-  captions: string
-  poster: string
 }
 
-export async function Video({ video, captions, poster }: Prop) {
+export async function Video({ video }: Prop) {
   const { default: src } = await import(`@/videos/${video}`)
   return <StyledVideo controls preload="lazy" src={src}></StyledVideo>
 }
