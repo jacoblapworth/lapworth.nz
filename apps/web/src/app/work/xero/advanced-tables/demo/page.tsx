b146333050e249816b7b90583658abc305b69d68
J <jacob@lapworth.nz>
2025-11-13 21:42:29 +0000

    fix

--- apps/web/src/app/work/xero/advanced-tables/demo/page.tsx
+++ apps/web/src/app/work/xero/advanced-tables/demo/page.tsx
@@ -1,5 +1,12 @@
 'use client'
 
 import { DataTableExample } from '@lapworth/xero/examples'
+import { Suspense } from 'react'
 
-export default DataTableExample
+export default function Page() {
+  return (
+    <Suspense>
+      <DataTableExample />
+    </Suspense>
+  )
+}
--- packages/xero/src/components/DataTable/data-table-search.tsx
+++ packages/xero/src/components/DataTable/data-table-search.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import type { RowData, Table } from '@tanstack/react-table'
 import { useState } from 'react'
 import { Search } from '../Search'
