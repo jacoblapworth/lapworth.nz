53ae50b8ee5047bb579baf70ef4778c32969daae
J <jacob@lapworth.nz>
2026-05-20 14:58:24 +0100

    update vite (#3069)
    * Update about
    
    * Add linked project
    
    * Update recipes
    
    * chore: update node version
    
    * content fixes
    
    * Format
    
    * fix storybook build issue
    
    * chore: update turbo
    
    * chore: update deps
    
    * fix: xero compile memory crash
    
    * fix: musickit types
    
    * fix
    
    * fix: musickit
    
    * chore: hoist deps
    
    * fix: markdown types
    
    * fix: linting
    
    * fix: linting
    
    * test: fix cypress
    
    * fix
    
    * chore: npm
    
    * chore: next-video
    
    * chore: deps
    
    * chore: sharp
    
    * wip
    
    * fix: storybook build timeout
    
    * fix: chromatic
    
    * ci: turbosnap

---
+++ apps/web/vercel.ts
@@ -0,0 +1,7 @@
+import type { VercelConfig } from '@vercel/config/v1'
+export const config: VercelConfig = {
+  github: {
+    silent: true,
+  },
+  relatedProjects: ['prj_pq97s4WRSr2GNFQZSvlP9NAaycGV'],
+}
